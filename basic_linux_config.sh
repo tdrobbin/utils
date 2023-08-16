964e5eb192cb27b8d5e0f98aa3fa0bb1bcf14d31
tdrobbin <tylerdrobbin@gmail.com>
2023-08-16 00:49:39 -0400

    Update basic_linux_config.sh

--- basic_linux_config.sh
+++ basic_linux_config.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get install -y build-essential clang gcc g++ cmake libssl-dev libssh-dev htop libffi-dev
+sudo apt-get update && sudo apt-get install -y build-essential clang gcc g++ cmake libssl-dev libssh-dev htop libffi-dev libpq-dev
